a597771d8c15df161533ceb1a5d390d905610d2f
soong-e <yujelee@student.42seoul.kr>
2022-09-13 15:18:49 +0900

    modify struct

--- bonus/server_bonus.h
+++ bonus/server_bonus.h
@@ -6,22 +6,17 @@
 /*   By: yujelee <yujelee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 20:30:48 by yujelee           #+#    #+#             */
-/*   Updated: 2022/09/11 19:29:09 by yujelee          ###   ########seoul.kr  */
+/*   Updated: 2022/09/13 15:18:33 by yujelee          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SERVER_BONUS_H
 # define SERVER_BONUS_H
 
-# define WAIT	0
-# define READY	1
-
 typedef struct s_box
 {
 	int		sum;
 	int		phase;
-	char	*str;
-	int		len;
 	int		client_pid;
 }	t_box;
 
